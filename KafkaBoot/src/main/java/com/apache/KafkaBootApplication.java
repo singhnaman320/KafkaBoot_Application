28e8c79b3c989afe2dd06a95019b2a6b0d43fec9
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-11 17:04:10 +0530

    Spring Boot + Kafka project basic setup

---
+++ KafkaBoot/src/main/java/com/apache/KafkaBootApplication.java
@@ -0,0 +1,13 @@
+package com.apache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBootApplication.class, args);
+	}
+
+}
