8455fedec45888f8b6182ca736f2c9dac1284aff
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-11 19:30:31 +0530

    Addiing Topic

--- KafkaBoot/src/main/java/com/apache/config/KafkaConfigTopic.java
+++ KafkaBoot/src/main/java/com/apache/config/KafkaConfigTopic.java
@@ -1,12 +1,14 @@
 package com.apache.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfigTopic {
 
+	@Bean
 	public NewTopic topicOne() {
 		
 		return TopicBuilder.name("springTopic")    // here only we can create partitions by .partitions(value)
