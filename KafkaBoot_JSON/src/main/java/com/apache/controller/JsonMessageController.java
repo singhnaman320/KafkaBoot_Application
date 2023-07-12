69e4efade13c3177bf2e466d7c940ff1e44c8568
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-12 18:02:25 +0530

    Adding JsonMessageController

--- KafkaBoot_JSON/src/main/java/com/apache/controller/JsonMessageController.java
+++ KafkaBoot_JSON/src/main/java/com/apache/controller/JsonMessageController.java
@@ -1,8 +1,12 @@
 package com.apache.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.apache.model.User;
 import com.apache.services.JsonKafkaProducerService;
 
 @RestController
@@ -11,10 +15,14 @@ public class JsonMessageController {
 
 	private JsonKafkaProducerService jsonKafkaProducerService;
 
+	// We will use constructor based dependency injection
 	public JsonMessageController(JsonKafkaProducerService jsonKafkaProducerService) {
 		super();
 		this.jsonKafkaProducerService = jsonKafkaProducerService;
 	}
 	
-	
+	@PostMapping("/publish")
+	public ResponseEntity<String> publishMessage( @RequestBody User user){
+		
+	}
 }
