722a81f8a6ccaa68da57551ff5385b1f5b88bf76
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-12 15:04:58 +0530

    Addiing Kafka Consumer Service

--- KafkaBoot/src/main/java/com/apache/services/KafkaConsumerService.java
+++ KafkaBoot/src/main/java/com/apache/services/KafkaConsumerService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumerService {
 
-	@KafkaListener
+	@KafkaListener(topics = "stringTopic")
 	public void consume(String message) {
 		
 	}
