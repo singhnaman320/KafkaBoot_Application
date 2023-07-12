e0eec7c3ebd48a47c29af11f36dea90959869e66
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-12 13:48:33 +0530

    Addiing Message COntroller

--- KafkaBoot/src/main/java/com/apache/controller/MessageController.java
+++ KafkaBoot/src/main/java/com/apache/controller/MessageController.java
@@ -1,11 +1,32 @@
 package com.apache.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.apache.services.KafkaProducerService;
 
 @RestController
 @RequestMapping("/api/kafka")
 public class MessageController {
 
+	private KafkaProducerService kafkaProducerService;
+
+	// Here only one constructor so we can ignore @Autowired
+	public MessageController(KafkaProducerService kafkaProducerService) {
+		super();
+		this.kafkaProducerService = kafkaProducerService;
+	}
+	
+	//http://localhost:8088/api/kafka/publish?message=first Message 
+	@PostMapping("/publish")
+	public ResponseEntity<String> publishMessage(@RequestParam("message") String message){
+		
+		kafkaProducerService.sendMessage(message);
+		
+		return ResponseEntity.ok("Message Published.!");
+	}
 	
 }
