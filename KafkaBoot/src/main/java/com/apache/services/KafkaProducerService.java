487aec3a569e83e684b4517ae7c7d5fe6706e9b8
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-12 12:09:33 +0530

    Addiing Producer

--- KafkaBoot/src/main/java/com/apache/services/KafkaProducerService.java
+++ KafkaBoot/src/main/java/com/apache/services/KafkaProducerService.java
@@ -1,6 +1,17 @@
 package com.apache.services;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaProducerService {
 
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+		super();
+		this.kafkaTemplate = kafkaTemplate;
+	}
+	
+	
 }
