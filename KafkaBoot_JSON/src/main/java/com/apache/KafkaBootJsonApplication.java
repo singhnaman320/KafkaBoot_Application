4f7c91380ad3040e61a134801d3cb90c5454e14a
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-12 15:32:59 +0530

    Addiing KafkaBoot_JSON basic structure

---
+++ KafkaBoot_JSON/src/main/java/com/apache/KafkaBootJsonApplication.java
@@ -0,0 +1,13 @@
+package com.apache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBootJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBootJsonApplication.class, args);
+	}
+
+}
