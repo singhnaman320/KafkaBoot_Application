da7a4cc380415da28fe29752173701b2dadbb3f0
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-12 12:41:35 +0530

    Addiing Message COntroller

--- KafkaBoot/src/main/java/com/apache/controller/MessageController.java
+++ KafkaBoot/src/main/java/com/apache/controller/MessageController.java
@@ -1,5 +1,11 @@
 package com.apache.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
 public class MessageController {
 
+	
 }
