c22ab790c91db23b0a92323b567ba1a2d67b3df6
Naman Kumar Singh <singhnaman320@gmail.com>
2023-07-12 12:17:48 +0530

    Addiing Producer Service

--- KafkaBoot/src/main/java/com/apache/services/KafkaProducerService.java
+++ KafkaBoot/src/main/java/com/apache/services/KafkaProducerService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaProducerService {
 	
+	// Log message
 	private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerService.class);
 
 	private KafkaTemplate<String, String> kafkaTemplate;
